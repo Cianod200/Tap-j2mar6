2779769f0e9085b42a338bbdde4a9637dc6af46e
Cianod200 <cian.odonovan@aig.com>
2025-03-06 16:29:22 +0000

    session end #

---
+++ td/src/main/java/com/tap/Main.java
@@ -0,0 +1,11 @@
+package com.tap;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        String[] ar = {"tom", "jack,", "anna"};
+       C_ar.main(args);
+         C_ar.arrayEg(ar);
+    }
+}
